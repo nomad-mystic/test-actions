e250552d6a77ebf05e00f4942ef998f0b3e09b19
Keith Murphy <keith@flickerbox.com>
2020-11-20 17:35:21 -0800

    Testing commit 1

--- .github/utils/test-actions.js
+++ .github/utils/test-actions.js
@@ -4,7 +4,10 @@ const testGetVariable = () => {
 
   return new Promise((resolve, reject) => {
 
+    console.log('process.env');
     console.log(process.env);
+
+    console.log('process.env["GITHUB_ENV"]');
     console.log(process.env['GITHUB_ENV']);
 
     core.exportVariable('TEST_VARIABLE', '100');
