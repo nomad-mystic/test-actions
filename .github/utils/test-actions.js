0e7d8ccee44e7a90a3f78bfb0a80df6ff9e1e71a
Keith Murphy <keith@flickerbox.com>
2020-11-20 17:32:19 -0800

    Init commit

---
+++ .github/utils/test-actions.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+
+const testGetVariable = () => {
+
+  return new Promise((resolve, reject) => {
+
+    console.log(process.env);
+    console.log(process.env['GITHUB_ENV']);
+
+    core.exportVariable('TEST_VARIABLE', '100');
+
+  });
+
+};
+
+testGetVariable().catch((err) => {
+  console.log(err);
+});
